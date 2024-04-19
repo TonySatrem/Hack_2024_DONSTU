ecff0f22a9bec3132f8ac1688fba142db074dcce
Aleksandr Krainukov <krainukovaleksandr1@gmail.com>
2024-04-20 01:52:15 +0300

    сделал участников команды

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,12 +7,11 @@ import PersonalAccount from "./pages/PersonalAccount/PersonalAccount";
 function App() {
   return (
     <div className="App">
-        <Router>
           <Routes>
             <Route path="/" element={<Home />} />
               <Route path="/personal" element={<PersonalAccount />} />
           </Routes>
-        </Router> 
+
     </div>
   );
 }
--- frontend/src/pages/PersonalAccount/PersonalAccount.tsx
+++ frontend/src/pages/PersonalAccount/PersonalAccount.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import TeamMembers from "../../components/TeamMembers/TeamMembers";
 
 export default function PersonalAccount() {
     return (
         <>
-
+<TeamMembers/>
         </>
     )
 }
