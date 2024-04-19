b43a1095ee19d270897f230fe616eb5acb25401e
Aleksandr Pogorelov <apogore@sfedu.ru>
2024-04-19 23:13:42 +0300

    Add Buttons

--- frontend/src/components/Carousel/Carousel.js
+++ frontend/src/components/Carousel/Carousel.js
@@ -3,26 +3,43 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Card, CardContent, Typography } from '@mui/material';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';    
+import Button from '@mui/material/Button';
 
 const Carousel = ({ cards }) => {
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  };
-  
-
-  return (
-    <Slider {...settings}>
-      {cards.map((card, index) => (
-        <div key={index}>
-          {card}
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      };
+    
+      const sliderRef = React.useRef();
+    
+      const goToPrev = () => {
+        sliderRef.current.slickPrev();
+      };
+    
+      const goToNext = () => {
+        sliderRef.current.slickNext();
+      };
+    
+      return (
+        <div>
+          <Slider ref={sliderRef} {...settings}>
+            {cards.map((card, index) => (
+              <div key={index}>
+              {card}
+              </div>
+            ))}
+          </Slider>
+          <Button onClick={goToPrev}><NavigateBeforeIcon /></Button>
+          <Button onClick={goToNext}><NavigateNextIcon /></Button>
         </div>
-      ))}
-    </Slider>
   );
 };
 
