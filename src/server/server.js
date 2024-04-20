3a92f328897babd5b515961881f4f684651c56fd
TonySatrem <flewdash777@gmail.com>
2024-04-20 19:37:52 +0300

    db init, refactor

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,7 @@
 import express from "express"
 import cors from "cors"
+import { getFile } from "./lib/getFile.js"
+import getContentType from "./lib/getContentType.js"
 
 const app = express()
 
@@ -9,6 +11,19 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
+})
+
+app.get("/", (req, res) => {
+  const { file, path } = getFile({
+    isIndex: true
+  })
+  
+  res.set({
+      'Content-Type': getContentType(path),
+      'Content-Length': Buffer.byteLength(file)
+  })
+  
+  res.sendFile(path)
 })
 
 const clientEndpoints = /.+\.(html|css|js|png|svg|ico|ttf|oft|woff|woff2)/
@@ -32,3 +47,9 @@ app.get(clientEndpoints, (req, res) => {
     res.end()
   }
 })
+
+const PORT = process.env.HTTP_PORT || 8080
+const HOSTNAME = process.env.HTTP_HOSTNAME || '127.0.0.1'
+
+app.listen(PORT, HOSTNAME, () => 
+  console.log(`server is listening at http://${HOSTNAME}:${PORT}`))
