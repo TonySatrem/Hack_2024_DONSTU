2f59940a7948a4c9258ff889f386067b564dedd9
TonySatrem <flewdash777@gmail.com>
2024-04-21 12:13:30 +0300

    fix db queries

--- src/server/db/models/dbQuestion.js
+++ src/server/db/models/dbQuestion.js
@@ -1,21 +1,20 @@
 import db from "../db.js"
 import tables from "../enums/tables.js"
 
-const table = db.table(tables.question)
 
 export async function addQuestion({ fullName, email, text, participantId }) {
-    return (await table
+    return (await db.table(tables.question)
         .returning('questionId')
         .insert({ fullName, email, text, participantId }))[0]
 }
 
 export async function getById({ questionId }) {
-    return (await table
+    return (await db.table(tables.question)
         .where({ questionId })[0])
 }
 
 export async function getAllQuestions({ participantId }) {
-    return (await table
+    return (await db.table(tables.question)
         .select()
         .where({ participantId })) 
 }
